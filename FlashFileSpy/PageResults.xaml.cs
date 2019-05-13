c5bf23c3f4cc07b8f2ace2f059471f0020be2d4e
David Hodges <dhodges@outermost.com>
2019-05-13 15:21:33 -0400

    Add project files.

---
+++ FlashFileSpy/PageResults.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlashFileSpy
+{
+    /// <summary>
+    /// Interaction logic for PageResults.xaml
+    /// </summary>
+    public partial class PageResults : Page
+    {
+        public PageResults()
+        {
+            InitializeComponent();
+            displayresults();
+        }
+
+        private void displayresults()
+        {
+            var listFlashFiles = (App.Current as App).listFlashFiles;
+            foreach (var sFlashFilePath in listFlashFiles)
+            {
+                txtBlock.Inlines.Add($"{sFlashFilePath}\n");
+
+            }
+        }
+    }
+}
---
+++ FlashFileSpy/PageStart.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace FlashFileSpy
+{
+    /// <summary>
+    /// Interaction logic for PageStart.xaml
+    /// </summary>
+    public partial class PageStart : Page
+    {
+        string[] extensions = new[] { ".swf", ".fla", ".flv" };
+        public PageStart()
+        {
+            InitializeComponent();
+        }
+        private void onDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                //Debug.WriteLine("Got Files");
+                //foreach (var file in files)
+                //{
+                //    Debug.WriteLine($"  File={file}");
+                //}
+            }
+        }
+
+        private void onDropRectangle(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var file in files)
+                {
+                    Debug.WriteLine($"  File={file}");
+                }
+
+                foreach (string file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    if (fi.Attributes == FileAttributes.Directory)
+                    {
+                        // it's a folder
+                        // traverse the folder structure and scan the contents for flash files
+                        Debug.WriteLine("It's a folder");
+                        DirectoryInfo di = new DirectoryInfo(fi.FullName);
+                        if (folderScan(di, false))
+                        {
+                            MessageBox.Show($"{(App.Current as App).listFlashFiles.Count} Flash file(s) found");
+                            // list of files is in listFlashFiles
+                        }
+
+                    }
+                    else if (fi.Attributes == FileAttributes.Archive || fi.Extension.ToLower() == ".zip")
+                    {
+                        // it's a zip file
+                        Debug.WriteLine("It's a zip file");
+                        using (ZipArchive archive = ZipFile.OpenRead(fi.FullName))
+                        {
+                            foreach (ZipArchiveEntry sZippedFile in archive.Entries)
+                            {
+                                if (bIsFlashFile(sZippedFile.FullName))
+                                {
+                                    (App.Current as App).listFlashFiles.Add(sZippedFile.FullName);
+
+                                }
+                            }
+                        }
+                        if ((App.Current as App).listFlashFiles.Count > 0)
+                        {
+                            MessageBox.Show($"{(App.Current as App).listFlashFiles.Count} Flash file(s) found");
+
+                        }
+                    }
+                    else
+                    {
+                        //it's a regular filefile
+                        // todo check the extension
+                        Debug.WriteLine("It's a regular file");
+                        if (bIsFlashFile(fi))
+                        {
+                            MessageBox.Show("Flash file found");
+                        }
+                    }
+                }
+                NavigationService ns = this.NavigationService;
+                Uri uri = new Uri("PageResults.xaml", UriKind.Relative);
+
+                // Get the navigation service that was used to 
+                // navigate to this page, and navigate to 
+                // AnotherPage.xaml
+                this.NavigationService.Navigate(uri);
+
+            }
+        }
+
+        private void onDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+
+                e.Effects = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+        private bool bIsFlashFile(FileInfo fi)
+        {
+            string s = fi.Extension.ToLower();
+            if (extensions.Contains(s))
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool bIsFlashFile(string sPath)
+        {
+            foreach (string sExt in extensions)
+            {
+                if (sPath.EndsWith(sExt, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool folderScan(DirectoryInfo di, bool bContainsFlash)
+        {
+            bool b = bContainsFlash;
+            FileInfo[] filesFiltered =
+                           di.EnumerateFiles()
+                                .Where(f => extensions.Contains(f.Extension.ToLower()))
+                                .ToArray();
+            foreach (FileInfo f in filesFiltered)
+            {
+                (App.Current as App).listFlashFiles.Add(f.FullName);
+                b = true;
+            }
+            var diSub = di.EnumerateDirectories();
+            foreach (DirectoryInfo dii in diSub)
+            {
+                b = folderScan(dii, b);
+            }
+            return b;
+        }
+
+        private void btnFileExplorer_onClick(object sender, RoutedEventArgs e)
+        {
+            // for opening a zip file
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.DefaultExt = "zip";
+            openFileDialog.Filter = "zip files(*.zip)|*.zip|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string sPath = openFileDialog.FileName;
+                FileInfo fi = new FileInfo(sPath);
+                using (ZipArchive archive = ZipFile.OpenRead(fi.FullName))
+                {
+                    foreach (ZipArchiveEntry sZippedFile in archive.Entries)
+                    {
+                        if (bIsFlashFile(sZippedFile.FullName))
+                        {
+                            (App.Current as App).listFlashFiles.Add(sZippedFile.FullName);
+
+                        }
+                    }
+                }
+                if ((App.Current as App).listFlashFiles.Count > 0)
+                {
+                    MessageBox.Show($"{(App.Current as App).listFlashFiles.Count} Flash file(s) found");
+
+                }
+
+            }
+        }
+
+        private void btnFolderExplorer_onClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string sPath = folderBrowserDialog.SelectedPath;
+                DirectoryInfo di = new DirectoryInfo(sPath);
+                if (folderScan(di, false))
+                {
+                    MessageBox.Show($"{(App.Current as App).listFlashFiles.Count} Flash file(s) found");
+                    // list of files is in listFlashFiles
+                }
+            }
+
+        }
+    }
+}
